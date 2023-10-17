73c85f5ef929bf2067c4586f11d738c80cb3b16c
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-10-17 15:42:07 +0200

    changes to first Puzzle

---
+++ src/com/fredrikEiserman/demo/Puzzle1/AppRunner.java
@@ -0,0 +1,9 @@
+package com.fredrikEiserman.demo.Puzzle1;
+
+public class AppRunner {
+    public static void main(String[] args) {
+
+        ShoppingList sHL = new ShoppingList();
+
+    }
+}
--- src/com/fredrikEiserman/demo/Puzzle1/ShoppingList.java
+++ src/com/fredrikEiserman/demo/Puzzle1/ShoppingList.java
@@ -10,12 +10,55 @@ public class ShoppingList {
 
     private List<String> shoppingList = new ArrayList<>();
 
+    public ShoppingList(){
+        runShoppingList();
+        printEndingMessage();
+    }
+
+    private void printMeny(){
+        System.out.println("Enter 1 to add to list \nEnter 2 to remove from list \nEnter 3 to show list \nEnter 4 to exit");
+    }
+
+    private void runShoppingList(){
+
+        while (true){
+            System.out.println("");
+            printMeny();
+            String choice = inputString("Enter your choice");
+            switch(choice){
+                case "1":
+                    addProduct();
+                    break;
+                case "2":
+                    removeProduct();
+                    break;
+                case "3":
+                    printList();
+                    break;
+                case"4":
+                    return;
+                default:
+                    System.out.println("Error: not an allowed option");
+                    break;
+            }
+        }
+
+    }
+
+    private void printEndingMessage(){
+        System.out.println("Bye bye");
+    }
+
     public void addProduct(){
-        shoppingList.add(inputString("Enter product to add into list").toLowerCase());
+        String temp = inputString("Enter product to add into list").toLowerCase();
+        shoppingList.add(temp);
+        System.out.println(temp + " was added");
     }
 
     public void removeProduct(){
-        shoppingList.remove(inputString("Enter product to remove from list").toLowerCase());
+        String temp = inputString("Enter product to remove from list").toLowerCase();
+        shoppingList.remove(temp);
+        System.out.println(temp + " was removed");
     }
 
     public void printList(){
